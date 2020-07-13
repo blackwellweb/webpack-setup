3a8898cc7508f2c7887cba173fd085fe7ab151e2
Paul Blackwell <blackwellpaul34@gmail.com>
2020-07-13 18:34:27 +0100

    Splitting Dev & Production

--- webpack.common.js
+++ webpack.common.js
@@ -4,13 +4,7 @@ const path = require('path');
 
 
 module.exports = {
-    mode: 'development',
-    devtool: 'none',
     entry: './src/index.js',
-    output: {
-        filename: 'main.[contentHash].js',
-        path: path.resolve(__dirname, 'dist')
-    },
     plugins: [new HtmlWebpackPlugin({
         template: "./src/template.html"
     })
