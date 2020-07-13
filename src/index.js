5b1f2aac42fc2e9fde5dff388fde0d2e49b697f4
Paul Blackwell <blackwellpaul34@gmail.com>
2020-07-13 18:07:33 +0100

    Cache Busting and Plugins

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import "./main.scss";
 import { AlertService } from './app/alert.service';
 import { ComponentService } from './app/component.service';
 
-
 const alertService = new AlertService();
 const componentService = new ComponentService();
 
